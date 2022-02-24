5e7d82af439c3d0754f14248b74592b54de3d361
shijuju0411 <56331579+shijuju0411@users.noreply.github.com>
2022-02-24 18:17:32 +0900

    2.24まで

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 import "./styles.css";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
@@ -16,17 +14,7 @@ export default function App() {
         <Link to="/page2">Page2</Link>
         <br />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
